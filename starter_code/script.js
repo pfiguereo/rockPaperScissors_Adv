b7482740dea5cb7ff62e4df8ab46db4f46cc880a
pfiguereo <pfiguereo@cristoreyny.org>
2015-11-19 13:17:47 +0000

    Sarasvati

--- starter_code/script.js
+++ starter_code/script.js
@@ -3,21 +3,40 @@ var aiPoint = 0;
 
 // This function returns the selection of the computer
 function getAISelection() {
-    //TODO: randomly choose between 'rock', 'paper', or 'scissors'
+    var options = ["rock","paper","scissors"];
+    var pickOption = Math.floor(Math.random()*3);
+    console.log(pickOption);
+    return options[pickOption];
+    
 }
 
 // This function picks the winner
 function pickWinner(userValue, aiValue) {
-    //TODO: pick the correct winner: user or ai
-    //TODO: Add one point for the winner
-}
+    if (userValue == aiValue)
+        return "draw";
+    if ( userValue == "rock" &&  aiValue == "paper")
+    return "ai";
+    if ( userValue == "rock" &&  aiValue == "scissors")
+    return "user";
+    if ( userValue == "paper" &&  aiValue == "rock")
+    return "user";
+    if ( userValue == "paper" &&  aiValue == "scissors")
+    return "ai";
+    if ( userValue == "scissors" && aiValue == "paper")
+    return "user";
+    if ( userValue == "scissors" && aiValue == "rock")
+    return "ai";
+}  
 
 // This function sets the scoreboard with the correct points
 function setScore() {
+$("#userPoint").text(userPoint);
+$("#aiPoint").text(aiPoint);
 
 }
 
-// This function captures the click and picks the winner
+//This function captures the click and picks the winner
+
 function evaluate(evt) {
     var userValue = evt.target.getAttribute('id');
     var aiValue = getAISelection();
@@ -25,15 +44,22 @@ function evaluate(evt) {
     var winner = pickWinner(userValue, aiValue);
 
     if ( 'user' === winner ) {
+        userPoint++;
         $('#message').delay(50).text('You have won!, Click a box to play again');
     } else if ( winner === 'draw' ) {
         $('#message').delay(50).text('Draw! Click a box to play again');
     } else {
         $('#message').delay(50).text('You have lost!, Click a box to play again');
+        aiPoint++;
     }
+
+    setScore()
 }
 
 // This function runs on page load
 $(document).ready(function(){
-
+    $(".token").click(function(evt){
+        evaluate(evt);
+    });
 });
+//////////WORK ON KEEPING THE SCORE/////////////
